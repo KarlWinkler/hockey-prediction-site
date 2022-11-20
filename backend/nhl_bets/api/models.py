5b049f1dd94bdbd66f799401a9533e3185dbe027
karlWinkler <karlw200@gmail.com>
2022-11-19 19:14:56 -0700

    create models

--- backend/nhl_bets/api/models.py
+++ backend/nhl_bets/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Game(models.Model):
+    date = models.DateField()
+    home_team = models.CharField(max_length=50)
+    away_team = models.CharField(max_length=50)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    result_in = models.IntegerField() # 0 = regulation, 1 = overtime, 2 = shootout
+
+    def __str__(self):
+        return self.home_team + ' vs ' + self.away_team
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=3)
+    nhl_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Bet(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bet_amount = models.IntegerField() # in points
+    result = models.CharField(max_length=50) # win, lose, pending
+    team = models.CharField(max_length=50) # home or away
+
+    def __str__(self):
+        return self.user + ' bet ' + self.home_score + ' - ' + self.away_score + ' on ' + self.game
+
---
+++ backend/nhl_bets/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path('games', views.get_games, name='games'),
+    # path('games/<string:date>', views.get_game_by_date, name='games_by_date'),
+    # path('game/<integer:id>', views.get_game, name='game'),
+    # path('game/<integer:id>/bets', views.get_game_bets, name='game_bets'),
+    # path('bets', views.get_bets, name='bets'),
+    # path('bet/<integer:id>', views.get_bets, name='bet'),
+]
