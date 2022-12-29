2630dc135b113a3c215e12504de5f4bec81ae89b
karlWinkler <karlw200@gmail.com>
2022-12-29 14:47:41 -0700

    put lose against streaks on frontend

--- frontend/nhl_bets/src/Pages/Trends.js
+++ frontend/nhl_bets/src/Pages/Trends.js
@@ -6,10 +6,12 @@ import '../styles/trends.scss'
 const Trends = () => {
   let [lossStreaks, setLossStreaks] = useState(null)
   let [winStreaks, setWinStreaks] = useState(null)
+  let [loseAgainstStreaks, setLoseAgainstStreaks] = useState(null)
 
   useEffect(() => {
     getLossStreaks()
     getWinStreaks()
+    getLoseAgainstStreaks()
   }, [])
 
   let getLossStreaks = async () => {
@@ -24,11 +26,18 @@ const Trends = () => {
     setWinStreaks(data.streaks)
   }
 
+  let getLoseAgainstStreaks = async () => {
+    let response = await fetch('/api/bets/lose_against_streak?num_results=3')
+    let data = await response.json()
+    setLoseAgainstStreaks(data.streaks)
+  }
+
   return (
     <div className='Trends'>
       <h1>Trends</h1>
-      <TeamStreaks title={'Your Longest Losing Streaks'} list={lossStreaks} />
-      <TeamStreaks title={'Your Longest Winning Streaks'} list={winStreaks} />
+      <TeamStreaks title={'You keep picking these teams wrong'} list={lossStreaks} />
+      <TeamStreaks title={'You keep picking these teams correctly'} list={winStreaks} />
+      <TeamStreaks title={'You have bet against these teams and lost'} list={loseAgainstStreaks} />
     </div>
   )
 }
