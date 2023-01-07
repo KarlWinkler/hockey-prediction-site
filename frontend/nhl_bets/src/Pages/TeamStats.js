adca5ed1559ca8428f79f1a2796cb11c82a4afa4
karlWinkler <karlw200@gmail.com>
2023-01-07 16:07:53 -0700

    change color on app-wrapper to white

--- frontend/nhl_bets/src/Pages/TeamStats.js
+++ frontend/nhl_bets/src/Pages/TeamStats.js
@@ -21,8 +21,8 @@ const TeamStats = () => {
     <div>
       <h1>{stats?.name} Stats</h1>
       <p>bets: {stats?.total_bets}</p>
-      <p>wins: {stats.total_wins}</p>
-      <p>win %: {stats.win_percent}</p>
+      <p>wins: {stats?.total_wins}</p>
+      <p>win %: {stats?.win_percent}</p>
     </div>
   )
 }
