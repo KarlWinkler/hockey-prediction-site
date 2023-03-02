97d785b0ecd8837fe424bc1ab93c15d5a935b009
karlWinkler <karlw200@gmail.com>
2023-03-01 20:20:58 -0700

    add production settings

---
+++ backend/nhl_bets/nhl_bets/production_settings.py
@@ -0,0 +1,17 @@
+from settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'karlwinkler$hockey-predictions',
+        'USER': 'karlwinkler',
+        'PASSWORD': 'hockeypickerdb',
+        'HOST': 'karlwinkler.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
